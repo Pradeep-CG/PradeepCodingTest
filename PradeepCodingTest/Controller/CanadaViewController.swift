f238b919f45915cbdd9b3b8acc6fdf28e7587ec3
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-06-21 00:10:28 +0530

    Removed storyboard and default viewcontroller and some basic setup

---
+++ PradeepCodingTest/Controller/CanadaViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CanadaViewController.swift
+//  PradeepCodingTest
+//
+//  Created by Pradeep on 20/06/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class CanadaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
